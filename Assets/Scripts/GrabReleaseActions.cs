b34e7fe2d9040c8c2c3a0c5e81a8b584796a4c09
Zoilo Mercedes <zmercedes89@gmail.com>
2018-05-01 16:48:50 -0400

    updated GrabReleaseActions for grabbing/throwing

--- Assets/Scripts/GrabReleaseActions.cs
+++ Assets/Scripts/GrabReleaseActions.cs
@@ -3,25 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GrabReleaseActions : MonoBehaviour {
+	
+	LeftControllerInput leftInput;
+	RightControllerInput rightInput;
 
 	public float throwForce = 1.5f;
 
 	// Use this for initialization
 	void Awake () {
-		GameObject left = GameObject.Find("Controller (left)");
-		GameObject right = GameObject.Find("Controller (right)");
-
-		left.GetComponent<LeftControllerInput>().GrabAction += GrabObject;
-		left.GetComponent<LeftControllerInput>().ReleaseAction += ThrowObject;
-		right.GetComponent<RightControllerInput>().GrabAction += GrabObject;
-		right.GetComponent<RightControllerInput>().ReleaseAction += ThrowObject;
+		leftInput = GameObject.Find("Controller (left)").GetComponent<LeftControllerInput>();
+		rightInput = GameObject.Find("Controller (right)").GetComponent<RightControllerInput>();
 		
+		leftInput.GrabAction += GrabObject;
+		rightInput.GrabAction += GrabObject;
+		leftInput.GrabAction += PlaceOrThrow;
+		rightInput.GrabAction += PlaceOrThrow;
+	}
+	
+	void PlaceorThrow(Collider col, SteamVR_Controller.Device controller, bool placeOrThrow = false){
+		if(placeOrThrow)
+			PlaceObject(col, controller);
+		else
+			ThrowObject(col, controller);
 	}
 	
 	void GrabObject(Collider coll, SteamVR_Controller.Device controller, Transform t){
-		coll.transform.SetParent(t);     // make controller parent
+		coll.transform.SetParent(t);                        // make controller parent
 		coll.GetComponent<Rigidbody>().isKinematic = true;  // turn off physics
-		controller.TriggerHapticPulse(2000);				// vibrate controller
+		controller.TriggerHapticPulse(2000);		    // vibrate controller
 		Debug.Log("Grabbing object!");
 	}
 
@@ -34,7 +43,7 @@ public class GrabReleaseActions : MonoBehaviour {
 		Debug.Log("Released object!");
 	}
 
-	void PlaceObject(){
-		
+	void PlaceObject(Collider col, SteamVR_Controller.Device controller){
+		// do stuff
 	}
 }
