008d41b0e22670335052e93779283a0701a5f2a8
Zoilo Mercedes <zmercedes89@gmail.com>
2018-05-02 14:48:38 -0400

    updated controllerinput

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -7,19 +7,40 @@ public class ControllerInput : MonoBehaviour {
 	// controller references
 	private SteamVR_Controller.Device controller;
 	private SteamVR_TrackedObject trackedObj;
-	
-	// actions
-	public event Action<Collider, SteamVR_Controller.Device, Transform> GrabAction;
-	public event Action<Collider, SteamVR_Controller.Device, string> ReleaseAction;
-	
-	string[] grabableObjects = {"place","throw"};
 
-	void OnTriggerStay(Collider col){
-		if(grabableObjects.Contains(col.gameObject.tag)){
-			if(controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
-				ReleaseAction(col, controller, col.gameObject.tag);
-			else if(controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
-				GrabAction(col, controller, transform);
+	private string[] grabableObjects = {"place","throw"};
+
+	void ReleaseActionPicker(Collider col, SteamVR_Controller.Device controller, string tag){
+		switch (tag){
+			case "place":
+				PlaceObject(col,controller);
+				break;
+			case "throw":
+				ThrowObject(col,controller);
+				break;
+			default:
+				Debug.Log("Nothing here!");
+				break;
 		}
+	}
+	
+	void GrabObject(Collider col, SteamVR_Controller.Device controller, Transform t){
+		col.transform.SetParent(t);                        // make controller parent
+		col.GetComponent<Rigidbody>().isKinematic = true;  // turn off physics
+		controller.TriggerHapticPulse(2000);               // vibrate controller
+		Debug.Log("Grabbing object!");
+	}
+
+	void ThrowObject(Collider col, SteamVR_Controller.Device controller){
+		col.transform.SetParent(null);
+		Rigidbody rigidBody = col.GetComponent<Rigidbody>();
+		rigidBody.isKinematic = false;
+		rigidBody.velocity = controller.velocity * throwForce;
+		rigidBody.angularVelocity = controller.angularVelocity;
+		Debug.Log("Released object!");
+	}
+
+	void PlaceObject(Collider col){
+		col.transform.SetParent(null);
 	}
 }
