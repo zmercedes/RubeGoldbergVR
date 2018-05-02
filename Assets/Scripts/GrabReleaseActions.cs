c0fd528f78bf4a8122ce00f6d934ef08fc0342e2
Zoilo <zmercedes89@gmail.com>
2018-05-01 20:06:03 -0400

    updated readme / comment/polish pass

--- Assets/Scripts/GrabReleaseActions.cs
+++ Assets/Scripts/GrabReleaseActions.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections;
+﻿/* Zoilo Mercedes
+ * GrabReleaseActions
+ * Sets appropriate grab and release actions. Common code between both hands
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GrabReleaseActions : MonoBehaviour {
-	
+
 	LeftControllerInput leftInput;
 	RightControllerInput rightInput;
 
 	public float throwForce = 1.5f;
 
-	// Use this for initialization
 	void Awake () {
 		leftInput = GameObject.Find("Controller (left)").GetComponent<LeftControllerInput>();
 		rightInput = GameObject.Find("Controller (right)").GetComponent<RightControllerInput>();
@@ -20,6 +24,7 @@ public class GrabReleaseActions : MonoBehaviour {
 		rightInput.ReleaseAction += ReleaseActionPicker;
 	}
 	
+	// does appropriate release action based on gameobject tag
 	void ReleaseActionPicker(Collider col, SteamVR_Controller.Device controller, string tag){
 		switch (tag){
 			case "place":
@@ -34,16 +39,16 @@ public class GrabReleaseActions : MonoBehaviour {
 		}
 	}
 	
-	void GrabObject(Collider coll, SteamVR_Controller.Device controller, Transform t){
-		coll.transform.SetParent(t);                        // make controller parent
-		coll.GetComponent<Rigidbody>().isKinematic = true;  // turn off physics
-		controller.TriggerHapticPulse(2000);		    // vibrate controller
+	void GrabObject(Collider col, SteamVR_Controller.Device controller, Transform t){
+		col.transform.SetParent(t);                        // make controller parent
+		col.GetComponent<Rigidbody>().isKinematic = true;  // turn off physics
+		controller.TriggerHapticPulse(2000);               // vibrate controller
 		Debug.Log("Grabbing object!");
 	}
 
-	void ThrowObject(Collider coll, SteamVR_Controller.Device controller){
-		coll.transform.SetParent(null);
-		Rigidbody rigidBody = coll.GetComponent<Rigidbody>();
+	void ThrowObject(Collider col, SteamVR_Controller.Device controller){
+		col.transform.SetParent(null);
+		Rigidbody rigidBody = col.GetComponent<Rigidbody>();
 		rigidBody.isKinematic = false;
 		rigidBody.velocity = controller.velocity * throwForce;
 		rigidBody.angularVelocity = controller.angularVelocity;
@@ -51,6 +56,8 @@ public class GrabReleaseActions : MonoBehaviour {
 	}
 
 	void PlaceObject(Collider col, SteamVR_Controller.Device controller){
-		// do stuff
+		col.transform.SetParent(null);
+		Rigidbody rigidBody = col.GetComponent<Rigidbody>();
+		rigidBody.isKinematic = false;
 	}
 }
