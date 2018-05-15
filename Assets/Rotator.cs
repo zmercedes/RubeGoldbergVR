a559b5dd8dd68da47c4b430695f12edb91ff9bb3
Zoilo <zmercedes89@gmail.com>
2018-05-15 18:44:06 -0400

    cleanup

--- Assets/Rotator.cs
+++ Assets/Rotator.cs
@@ -1,16 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Rotator : MonoBehaviour {
 
 	Vector3 currentPos;
-	// Use this for initialization
+
 	void Start () {
 		currentPos = transform.parent.position;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		if(transform.parent.position != currentPos){
 			float mag = (currentPos - transform.parent.position).magnitude;
